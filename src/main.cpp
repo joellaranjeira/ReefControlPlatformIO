5e8ba25641b9be628d23cd6a800807bce2b21e05
joellaranjeira <118441245+joellaranjeira@users.noreply.github.com>
2026-04-13 19:22:05 -0300

    Bump firmware version to 1.1

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include <HTTPUpdate.h>
 #include <UniversalTelegramBot.h>
 
-#define FW_VERSION "1.0"
+#define FW_VERSION "1.1"
 #define MAX_HISTORY 720
 
 // ================= TELEGRAM =================
